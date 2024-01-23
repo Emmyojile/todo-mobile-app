ccde001888b6acfa6704e12f2dc1091c014bb006
Emmyojile <emmaojile99@gmail.com>
2024-01-23 15:13:57 +0100

    tabs and screens

--- app/(tabs)/home/index.js
+++ app/(tabs)/home/index.js
@@ -1,14 +1,91 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  Image,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React from "react";
+import { AntDesign } from "@expo/vector-icons";
 
 const index = () => {
+  const todos = [];
   return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
-}
+    <>
+      <View style={styles.topContainer}>
+        <Pressable style={styles.tabs}>
+          <Text style={{ color: "white", textAlign: "center" }}>All</Text>
+        </Pressable>
+        <Pressable style={styles.tabs}>
+          <Text style={{ color: "white", textAlign: "center" }}>Work</Text>
+        </Pressable>
+        <Pressable style={styles.tabs}>
+          <Text style={{ color: "white", textAlign: "center" }}>Personal</Text>
+        </Pressable>
+        <Pressable style={{ marginLeft: "auto" }}>
+          <AntDesign name="pluscircle" size={30} color="#007fff" />
+        </Pressable>
+      </View>
 
-export default index
+      <ScrollView style={{ flex: 1, backgroundColor: "white" }}>
+        <View style={{ padding: 10 }}>
+          {todos?.length > 0 ? (
+            <View></View>
+          ) : (
+            <View style={styles.addTab}>
+              <Image
+                style={{ width: 100, height: 100 }}
+                source={{
+                  uri: "https://cdn-icons-png.flaticon.com/128/2387/2387679.png",
+                }}
+              />
+              <Text
+                style={{
+                  fontSize: 16,
+                  marginTop: 15,
+                  fontWeight: "600",
+                  textAlign: "center",
+                }}
+              >
+                No Tasks for Today! add a task
+              </Text>
+              <Pressable style={{ marginTop: 15 }}>
+                <AntDesign name="pluscircle" size={30} color="#007fff" />
+              </Pressable>
+            </View>
+          )}
+        </View>
+      </ScrollView>
+    </>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default index;
+
+const styles = StyleSheet.create({
+  topContainer: {
+    marginHorizontal: 10,
+    marginVertical: 10,
+    alignItems: "center",
+    flexDirection: "row",
+    gap: 12,
+    borderColor: "#7cb9e8",
+  },
+  tabs: {
+    backgroundColor: "#7cb9e8",
+    paddingHorizontal: 10,
+    paddingVertical: 6,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  addTab: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 130,
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+});
