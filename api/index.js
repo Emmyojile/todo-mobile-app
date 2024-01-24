5b6ceab5bcc2c65579dfccfe40c2484dbb1f0b62
Emmyojile <emmaojile99@gmail.com>
2024-01-24 14:25:00 +0100

    authencation endpoints

--- api/index.js
+++ api/index.js
@@ -12,12 +12,46 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb+srv://emmyojile:emmy%401599@cluster0.baodx4z.mongodb.net/todo-app?retryWrites=true&w=majority").then(() => {
+mongoose
+  .connect(
+    "mongodb+srv://emmyojile:emmy%401599@cluster0.baodx4z.mongodb.net/todo-app?retryWrites=true&w=majority"
+  )
+  .then(() => {
     console.log("Connected to MongoDb");
-}).catch((error) => {
+  })
+  .catch((error) => {
     console.log("Error connecting to MongoDB");
-})
+  });
 
 app.listen(port, () => {
-    console.log("Server running on port " + port);
+  console.log("Server running on port " + port);
+});
+
+const User = require("./models/user");
+const Todo = require("./models/todo");
+
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    //check if email already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      console.log("Email already exists");
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const newUser = await User({
+      name,
+      email,
+      password,
+    });
+
+    await newUser.save();
+
+    res.status(202).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.log("Error: " + error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
