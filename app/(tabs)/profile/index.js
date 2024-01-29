a36af96aa10a3076de880e00bffbbe332fe4817f
Emmyojile <emmaojile99@gmail.com>
2024-01-29 11:44:31 +0100

    profile tab

--- app/(tabs)/profile/index.js
+++ app/(tabs)/profile/index.js
@@ -1,7 +1,29 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const index = () => {
+  const [completedTasks, setCompletedTasks] = useState(0)
+  const [pendingTasks, setPendingTasks] = useState(0)
+
+  const fetchTasksData = () => {
+    try {
+      const response = axios.get('http://10.0.2.2:5000/todos/count')
+      const {totalCompletedTodos,totalPendingTodos} = response.data;
+      
+      setCompletedTasks(totalCompletedTodos)
+      setPendingTasks(totalPendingTodos)
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+ fetchTasksData();
+  },[]);
+  console.log("comp", completedTasks);
+  console.log("pend", pendingTasks);
   return (
     <View>
       <Text>index</Text>
