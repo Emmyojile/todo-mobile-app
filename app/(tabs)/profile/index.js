e98aad9055f3fd0ee819f88165163b0fc1f4dda9
Emmyojile <emmaojile99@gmail.com>
2024-01-29 12:08:54 +0100

    profile tab

--- app/(tabs)/profile/index.js
+++ app/(tabs)/profile/index.js
@@ -1,36 +1,52 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import { Image, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const index = () => {
-  const [completedTasks, setCompletedTasks] = useState(0)
-  const [pendingTasks, setPendingTasks] = useState(0)
+  const [completedTasks, setCompletedTasks] = useState(0);
+  const [pendingTasks, setPendingTasks] = useState(0);
 
-  const fetchTasksData = () => {
+  const fetchTasksData = async () => {
     try {
-      const response = axios.get('http://10.0.2.2:5000/todos/count')
-      const {totalCompletedTodos,totalPendingTodos} = response.data;
-      
-      setCompletedTasks(totalCompletedTodos)
-      setPendingTasks(totalPendingTodos)
-      
-    } catch (error) {
-      console.log(error);
-    }
-  }
+      const response = await axios.get("http://10.0.2.2:5000/todos/count");
+      const { totalCompletedTodos, totalPendingTodos } = response.data;
 
-  useEffect(()=>{
- fetchTasksData();
-  },[]);
+      setCompletedTasks(totalCompletedTodos);
+      setPendingTasks(totalPendingTodos);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTasksData();
+  }, []);
   console.log("comp", completedTasks);
   console.log("pend", pendingTasks);
   return (
-    <View>
-      <Text>index</Text>
+    <View style={{ padding: 10, flex: 1, backgroundColor: "white" }}>
+      <View style={{flexDirection:"row", alignItems:"center",gap:10}}>
+        <Image
+          style={{ width: 60, height: 60, borderRadius: 30 }}
+          source={{
+            uri: "https://lh3.googleusercontent.com/ogw/ANLem4Zmk7fohWyH7kB6YArqFy0WMfXnFtuX3PX3LSBf=s64-c-mo",
+          }}
+        />
+        <View>
+          <Text style={{ fontSize: 16, fontWeight: "600" }}>
+            Kepp Plans for 15 Days
+          </Text>
+          <Text style={{ fontSize: 15, color: "gray", marginTop: 4 }}>
+            Select Categories
+          </Text>
+        </View>
+      </View>
+
+      <View></View>
     </View>
-  )
-}
+  );
+};
 
-export default index
+export default index;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
