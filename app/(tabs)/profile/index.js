2107c2098d6fdcfc83307530dbe7964f0f5381bf
Emmyojile <emmaojile99@gmail.com>
2024-01-29 13:21:15 +0100

    profile tab

--- app/(tabs)/profile/index.js
+++ app/(tabs)/profile/index.js
@@ -1,6 +1,7 @@
 import { Image, StyleSheet, Text, View } from "react-native";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { LineChart } from "react-native-chart-kit";
 
 const index = () => {
   const [completedTasks, setCompletedTasks] = useState(0);
@@ -42,7 +43,64 @@ const index = () => {
         </View>
       </View>
 
-      <View></View>
+      <View style={{marginVertical:12}}>
+        <Text>Tasks Overview</Text>
+        <View style={{ flexDirection: "row", alignItems: "center",marginVertical:10, gap:6 }}>
+          <View style={{ padding: 10, backgroundColor: "#89cff0" ,padding: 10,borderRadius:8,flex:1,justifyContent:"center",alignItems:"center" }}>
+          <Text style={{ textAlign: "center", fontSize: 16, fontWeight: "bold" }}>
+            {completedTasks} 
+          </Text>
+          <Text style={{marginTop:4}}>
+            Completed tasks
+          </Text>
+          </View>
+
+          <View style={{ padding: 10, backgroundColor: "#89cff0" ,padding: 10,borderRadius:8,flex:1,justifyContent:"center",alignItems:"center" }}>
+          <Text style={{ textAlign: "center", fontSize: 16, fontWeight: "bold" }}>
+            {pendingTasks} 
+          </Text>
+          <Text style={{marginTop:4}}>
+            pending tasks
+          </Text>
+          </View>
+        </View>
+
+        <LineChart
+        data={{
+          labels: ["Pending Tasks", "Completed Tasks"],
+          datasets: [
+            {
+              data: [pendingTasks, completedTasks],
+            },
+          ],
+        }}
+        width={Dimensions.get("window").width - 20} // from react-native
+        height={220}
+        // yAxisLabel="$"
+        // yAxisSuffix="k"
+        yAxisInterval={2} // optional, defaults to 1
+        chartConfig={{
+          backgroundColor: "#e26a00",
+          backgroundGradientFrom: "#fb8c00",
+          backgroundGradientTo: "#ffa726",
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+          propsForDots: {
+            r: "6",
+            strokeWidth: "2",
+            stroke: "#ffa726",
+          },
+        }}
+        bezier
+        style={{
+          borderRadius: 16,
+        }}
+      />
+      </View>
     </View>
   );
 };
